9053df1c85529fccfe51982623d86b0edc9e4c82
buraksenyurt <burakselimsenyurt@gmail.com>
2023-09-04 18:32:58 +0300

    Used display trait.

--- Samples/006 - some cookies/move_with_ref/src/main.rs
+++ Samples/006 - some cookies/move_with_ref/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 struct Player {
     id: i32,
     title: String,
@@ -10,6 +12,12 @@ impl Player {
     }
 }
 
+impl Display for Player {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {} ({})", self.id, self.title, self.point)
+    }
+}
+
 fn transfer_points(src_player: &Player, trg_player: &mut Player) {
     trg_player.point += src_player.point
 }
@@ -18,4 +26,6 @@ fn main() {
     let baltazar = Player::new(13, "baltazar the pirate".to_string(), 18);
     let mut dufrey = Player::new(32, "endi dufrey".to_string(), 5);
     transfer_points(&baltazar, &mut dufrey);
+    println!("{}", baltazar);
+    println!("{}", dufrey);
 }
